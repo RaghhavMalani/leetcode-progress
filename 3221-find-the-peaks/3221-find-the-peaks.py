fcf4c06916ef740e7ed47276ea96a33207e26ae8
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2025-07-31 12:35:26 +0530

    Time: 0 ms (100%), Space: 12.5 MB (46.54%) - LeetHub

---
+++ 3221-find-the-peaks/3221-find-the-peaks.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findPeaks(self, mountain):
+        """
+        :type mountain: List[int]
+        :rtype: List[int]
+        """
+        l = []
+        for i in range(1,len(mountain)-1):
+            if mountain[i] > mountain[i - 1] and mountain[i] > mountain[i + 1]:
+                l.append(i)
+            else:
+                continue
+
+        return l
