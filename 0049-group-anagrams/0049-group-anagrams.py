0e001639e0fb16c5e5ab7a980edbfc328b8378e5
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-06-02 22:07:56 +0530

    Time: 17 ms (26.25%), Space: 23.9 MB (18.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,14 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        map = defaultdict(list)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        for i in range(len(strs)):
-            count = [0] * 26
-            for char in strs[i]:
-                count[ord(char) - ord("a")] += 1
-
-            map[tuple(count)].append(strs[i])
-
-        return map.values()
+        hashmap = defaultdict(list)
         
-            
+        for word in strs:
+            count = [0] * 26
+
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+
+            hashmap[tuple(count)].append(word)
+
+        return list(hashmap.values())
