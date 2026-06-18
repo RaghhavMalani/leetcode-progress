635bb5f1ee7d086d9949cdb8a53ad53f08255567
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-06-18 11:35:08 +0530

    Time: 44 ms (84.34%), Space: 133.8 MB (14.3%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,13 +1,14 @@
+import java.util.*;
+
 class Node {
+    int key;
     int val;
     Node prev;
-    int key;
     Node next;
 
-    public Node(int key, int val)
-    {
-        this.val = val;
+    public Node(int key, int val) {
         this.key = key;
+        this.val = val;
         this.next = null;
         this.prev = null;
     }
@@ -17,58 +18,62 @@ class LRUCache {
     int cap;
     Node lru;
     Node mru;
-    HashMap <Integer, Node> cache;
+    HashMap<Integer, Node> cache;
 
     public LRUCache(int capacity) {
         this.cap = capacity;
-        lru = new Node(0,0);
-        mru = new Node(0,0);
+        this.cache = new HashMap<>();
+
+        lru = new Node(0, 0);
+        mru = new Node(0, 0);
+
         lru.next = mru;
         mru.prev = lru;
-        cache = new HashMap<>();
     }
-    private void insert(Node node){
+
+    private void insert(Node node) {
         Node prv = mru.prev;
+
         prv.next = node;
-        mru.prev = node;
-        node.next =  mru;
         node.prev = prv;
+
+        node.next = mru;
+        mru.prev = node;
     }
-    
-    private void delete(Node node){
+
+    private void delete(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 
     public int get(int key) {
-        if (cache.containsKey(key)){
+        if (cache.containsKey(key)) {
             Node node = cache.get(key);
+
             delete(node);
             insert(node);
+
             return node.val;
         }
+
         return -1;
     }
-    
+
     public void put(int key, int value) {
         if (cache.containsKey(key)) {
-            delete(cache.get(key));
+            Node oldNode = cache.get(key);
+            delete(oldNode);
         }
-        Node node = new Node(key,value);
+
+        Node node = new Node(key, value);
         insert(node);
         cache.put(key, node);
 
-        if (cache.size() > cap){
+        if (cache.size() > cap) {
             Node rem = lru.next;
+
             delete(rem);
             cache.remove(rem.key);
         }
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
