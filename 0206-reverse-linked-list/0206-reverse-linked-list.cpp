62270151a240643a1727b45ed7d19a5cf7ba59b0
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-07-01 10:51:44 +0530

    Time: 0 ms (100%), Space: 13.5 MB (41.41%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -3,21 +3,26 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode() : val(0), next(nullptrptr) {}
+ *     ListNode(int x) : val(x), next(nullptrptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        while (head){
-            ListNode* temp = head->next;
-            head->next = prev;
-            prev = head;
-            head = temp;
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+
+        while (curr != nullptr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+
+            prev = curr;
+            curr = temp;
         }
+
         return prev;
     }
 };
