1965c66fb259eba6c594629098fca0de8fd1bac4
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-08-04 15:52:16 +0530

    Time: 4 ms (64.71%), Space: 20.8 MB (47.15%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,33 @@
+from typing import List
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
 
         for course, prerequisite in prerequisites:
-            adj[prerequisite].append(course)
-            indegree[course] += 1
+            adj[course].append(prerequisite)
 
-        queue = deque()
+        state = [0] * numCourses
+
+        def dfs(course):
+            if state[course] == 1:
+                return False
+
+            if state[course] == 2:
+                return True
+
+            state[course] = 1
+
+            for prerequisite in adj[course]:
+                if not dfs(prerequisite):
+                    return False
+
+            state[course] = 2
+            return True
 
         for course in range(numCourses):
-            if indegree[course] == 0:
-                queue.append(course)
+            if state[course] == 0:
+                if not dfs(course):
+                    return False
 
-        completed_courses = 0
-
-        while queue:
-            course = queue.popleft()
-            completed_courses += 1
-            for next_course in adj[course]:
-                indegree[next_course] -= 1
-
-                if indegree[next_course] == 0:
-                    queue.append(next_course)
-
-        return completed_courses == numCourses
+        return True
