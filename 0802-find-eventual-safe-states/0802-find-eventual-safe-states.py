e329c8b441482b4d18fdf7ef2a6c6823654a94f0
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-08-05 23:42:46 +0530

    Time: 22 ms (92.23%), Space: 25.3 MB (92.37%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,17 +1,16 @@
+from typing import List
+
 class Solution:
     def dfsCheck(self, node, graph, vis, pathVis, check):
         vis[node] = 1
         pathVis[node] = 1
-        check[node] = 0
 
         for nbr in graph[node]:
-            if (not vis[nbr]):
+            if not vis[nbr]:
                 if self.dfsCheck(nbr, graph, vis, pathVis, check):
-                    check[node] = 0
                     return True
-                
+
             elif pathVis[nbr]:
-                check[node] = 0
                 return True
 
         check[node] = 1
@@ -20,20 +19,19 @@ class Solution:
 
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
+
         vis = [0] * n
         pathVis = [0] * n
         check = [0] * n
-        safenodes = []
-        for i in range(n):
-            if not vis[i]:
-                self.dfsCheck(i, graph, vis, pathVis, check)
-            
-        for i in range(n):
-            if check[i] == 1:
-                safenodes.append(i)
-        
-        return safenodes
 
-                
+        for node in range(n):
+            if not vis[node]:
+                self.dfsCheck(node, graph, vis, pathVis, check)
 
-        
+        safeNodes = []
+
+        for node in range(n):
+            if check[node] == 1:
+                safeNodes.append(node)
+
+        return safeNodes
