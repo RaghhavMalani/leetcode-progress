51e3af4ceeb743bb705de6c9b31959e115061ebf
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-08-23 22:44:27 +0530

    Time: 0 ms (100%), Space: 20.2 MB (36.63%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,23 +1,16 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        n = len(rooms)
-        vis = [0] * n
-        queue = deque()
-        start = 0
-        queue.append(start)
+        visited = set()
 
-        while queue:
-            current = queue.popleft()
-            vis[current] = 1
-            
-            for nbr in rooms[current]:
-                if vis[nbr] == 0:
-                    vis[nbr] = 1
-                    queue.append(nbr)
-                else:
-                    continue
+        def dfs(room):
+            if room in visited:
+                return
 
-        if 0 in vis:
-            return False
-        
-        return True
+            visited.add(room)
+
+            keys = rooms[room]
+            for key in keys:
+                dfs(key) 
+
+        dfs(0)
+        return len(visited) == len(rooms)
