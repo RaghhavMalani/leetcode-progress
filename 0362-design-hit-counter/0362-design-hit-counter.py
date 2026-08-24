c486afb196010dcab81c978efccf3c7c0e8f4426
Raghhav Malani <96712854+RaghhavMalani@users.noreply.github.com>
2026-08-24 15:57:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (40.73%) - LeetHub

---
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -0,0 +1,19 @@
+class HitCounter:
+
+    def __init__(self):
+        self.store = deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.store.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        
+        while self.store and self.store[0] <= timestamp - 300:
+            self.store.popleft()
+
+        return len(self.store)
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
